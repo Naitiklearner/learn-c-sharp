9fd938974d73d8c3159a434484eaef24ba2f9f92
Naitiklearner <ng4105864@gmail.com>
2025-01-24 15:44:02 +0530

    Initial commit

---
+++ eligiblity-to-vote/eligiblity-to-vote.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eligiblity_to_vote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your Password: ");
+            string Password =  Console.ReadLine() ?? "error";
+
+            if (Password == "naitik")
+            {
+                Console.Write("Enter your age: ");
+
+                int intage = Convert.ToInt32(Console.ReadLine());
+
+                if (intage >= 18)
+                    Console.WriteLine("you are eligible to vote");
+                else
+                    Console.WriteLine("you are not eligible to vote");
+                
+
+            }
+            else if (Password == "error")   
+                Console.WriteLine("Provide a password");
+            else
+                Console.WriteLine("Password incorrect");
+
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ table(multiplication)/table(multiplication)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace eligiblity_to_vote 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.Write("Enter the password: ");
+            string Password = Console.ReadLine() ?? "error";
+            
+            if (Password == "naitik")
+            {
+                Console.Write("Enter the number of which table you want to print: ");
+
+                int intnum = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 1; i <= 10; i++)
+                {
+                Console.WriteLine(Convert.ToString(intnum) + " * " + i + " = " + intnum * i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Password incorrect");
+            }
+            Console.WriteLine("Press any key to close the program");
+            Console.ReadKey();
+        }
+    }
+}
